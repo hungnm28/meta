b8cee9169381365ed2e4c058c28c66e093151917
Hung Nguyen Manh <hungnm@Hungs-MacBook-Pro.local>
2023-06-18 16:00:40 +0700

    f

--- src/Meta.php
+++ src/Meta.php
@@ -42,6 +42,7 @@ class Meta
         $this->description = $this->validateData(data_get($data, "description", $this->description));
         $this->url = data_get($data, "url", $this->url);
         $this->metas = data_get($data, "meta", $this->metas);
+        $this->links = data_get($data, "link", $this->links);
         $this->jsons = data_get($data, "json", $this->jsons);
         $this->breadcrumb = data_get($data, "breadcrumb", $this->breadcrumb);
     }
